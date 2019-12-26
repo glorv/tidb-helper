2271fa66a0ff3cb66e823aba9b1d4ad0d479354e
glorv <glorvs@163.com>
2019-12-26 18:54:50 +0800

    upd

--- scripts/build-toolkit.sh
+++ scripts/build-toolkit.sh
@@ -7,7 +7,7 @@ cd /build/importer && make release
 cd /build/tidb-lightning && make
 cd /build/tidb-tools && make build
 
-cd /build/pd/bin && cp pd-tso-bench /out
+cp /build/pd/bin/pd-tso-bench /out
 cd /build/tidb-lightning/bin && cp tidb-lightning tidb-lightning-ctl /out
 cp /build/tidb-tools/bin/sync_diff_inspector  /out
 cp /build/importer/target/release/tikv-importer /out
